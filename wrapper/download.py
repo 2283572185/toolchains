58eb3f18047f79cc9a5836b3e46299346f7ef6a7
24bit-xjkp <2283572185@qq.com>
2025-01-07 14:43:40 +0800

    feat(wrapper): 在wrapper目录下添包装脚本以允许直接执行
    1. 添加wrapper/download.py，允许在不安装工程的情况下直接通过源代码执行脚本

---
+++ wrapper/download.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pathlib
+import sys
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
+from toolchains.download import main
+
+main()
