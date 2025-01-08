ec0ce616a0f593754a518876fe1aaf3a7e4c61bb
24bit-xjkp <2283572185@qq.com>
2025-01-08 16:04:51 +0800

    docs(doc): 改进toolchains模块文档
    1. 删除多余子模块章节，使用__init__.py中包含的子模块
    2. 删除conf.py中添加模块路径的代码，使用poetry install后可以直接找到toolchains模块
    3. 为download_source和download添加注释

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,6 @@
-from sphinx_pyproject import SphinxConfig
 import pathlib
-import sys
+
+from sphinx_pyproject import SphinxConfig
 
 root_dir = pathlib.Path(__file__).parents[2]
 config = SphinxConfig(root_dir / "pyproject.toml", globalns=globals())
@@ -8,6 +8,3 @@ config = SphinxConfig(root_dir / "pyproject.toml", globalns=globals())
 project = name  # type:ignore
 for key in config:
     globals()[key] = config[key]
-
-
-sys.path.insert(0, str(root_dir))
